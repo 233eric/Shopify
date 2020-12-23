e6ebfbcbf86e80f16ce849638fa7727fae0f5fad
233eric <233ericthai@gmail.com>
2020-12-23 16:55:06 -0500

    added sll to api

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getMovies = async (search) => {
   try {
-    const response = await axios.get('http://www.omdbapi.com/?apikey=385f7099&', {
+    const response = await axios.get('https://www.omdbapi.com/?apikey=385f7099&', {
       params: {
         s: search
       }
