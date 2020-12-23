3d929888cd7bbebc8898e28233c5a488ddf4a16b
233eric <233ericthai@gmail.com>
2020-12-23 03:01:35 -0500

    try catch for axios get

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
 
 export const getMovies = async (search) => {
-  const response = await axios.get('http://www.omdbapi.com/?apikey=385f7099&', {
-  params: {
-    s: search
+  try {
+    const response = await axios.get('http://www.omdbapi.com/?apikey=385f7099&', {
+      params: {
+        s: search
+      }
+    })
+    console.log(response.data);
+    return response.data;
   }
-}).then(response => {
-    return response.data
-  }).catch(error => console.log(error));
-  return response;
+  catch (error) {
+    console.log(error);
+  }
 }
